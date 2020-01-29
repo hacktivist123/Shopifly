5cec7146c7d6f601046b175a82821786063281a9
hacktivist123 <akintayoshedrack@gmail.com>
2020-01-29 15:11:15 +0100

    broken hompeage into smaller components, finished styling menu item component

---
+++ src/components/menuItem/MenuItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './MenuItemStyles.scss'
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div  className={`${size} menu-item`}>
+  <div
+    className='background-image'
+    style={{
+    backgroundImage: `url(${imageUrl})`
+  }}/>
+        <div className='content'>
+          <h1 className='title'>{title.toUpperCase()}</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem
---
+++ src/pages/homepage/HompageComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './HomePageStyles.scss'
+import Directory from '../../components/Directory/Directory'
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory/>
+  </div>
+)
+export default HomePage;
