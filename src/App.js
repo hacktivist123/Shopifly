72841817be3e55a2bb82b180455badacf157d030
hacktivist123 <akintayoshedrack@gmail.com>
2020-01-29 01:32:28 +0100

    intial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HompageComonent'
+function App() {
+  return (
+    <div>
+            <HomePage/>
+    </div>
+  );
+}
+
+export default App;
