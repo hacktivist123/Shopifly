7e3d35bd2578e1f9c34e866e819043c9d3cef222
hacktivist123 <akintayoshedrack@gmail.com>
2020-02-05 17:21:30 +0100

    added Header(Navbar) component and links everywhere

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/HompageComponent'
 import ShopPage from './pages/shop/ShopPage'
-
+import Header from './components/Header/Header'
 function App() {
   return (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage} />
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './Header.scss'
+import {ReactComponent as Icon} from '../../assets/shop.svg'
+const Header = () => (
+  <div className='header'>
+    <Link to= '/' className='logo-container'>
+        <Icon className= 'logo'/>
+    </Link>
+    <div className='options'>
+      <Link to= '/shop' className='option'>
+          SHOP
+      </Link>
+      <Link to= '/contact' className='option'>
+          CONTACT
+      </Link>
+    </div>
+  </div>
+)
+export default Header
